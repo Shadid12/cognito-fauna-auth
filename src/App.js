13d715307e4a2ecb5038195abecf07af952946aa
shaque <shaque@omers.com>
2021-08-30 23:06:55 -0400

    added custom flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify, { Auth } from "aws-amplify";
+
+import {AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
 
 function App() {
+  Auth.configure({
+    authenticationFlowType: 'CUSTOM_AUTH'
+  });
+  const loginHere = async () => {
+    const user = await Auth.signIn(
+      'pigis34147@macauvpn.com', 
+      'pigis34147@macauvpn.com'
+    );
+    console.log('user', user);
+  }
+
+  const logoutHere = async () => {
+    await Auth.signOut();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <AmplifyAuthenticator>
+      <div className="App">
+        <h1>Fauna App</h1>
+        <button onClick={loginHere}>
+          Login
+        </button>
+        <button onClick={logoutHere}>
+          Logout
+        </button>
+        {/* <AmplifySignOut /> */}
+      </div>
+    // </AmplifyAuthenticator>
   );
 }
 
